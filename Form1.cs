57b59d4e93f77f9ab3e29b9e40eb38a55d5e5733
salticid <rbuenger@aerometric.com>
2013-09-30 13:23:35 -0500

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloGitHub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello Github";
+            label1.Visible = !label1.Visible;
+        }
+    }
+}
